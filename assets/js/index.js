fa2d735fd5c999307121f82f373c469fb9e67ce4
manoel <piloukos45@gmail.com>
2024-10-18 10:03:32 -0300

    feat: function remove classList menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,16 @@
 
 const menu_Toggle = document.querySelector('.menu-Toggle')
 const nav = document.querySelector('nav')
+const links = document.querySelectorAll('ul a')
 
 menu_Toggle.addEventListener('click', () => {
     menu_Toggle.classList.toggle('toggle')
     nav.classList.toggle('active')
+})
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menu_Toggle.classList.toggle('toggle')
+        nav.classList.toggle('active')
+    })
 })
